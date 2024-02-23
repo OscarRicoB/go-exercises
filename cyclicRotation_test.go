9e21508b3140aa1c9ccbe77a6b4f78b71954d5c7
Oscar Rico Barajas <oscarricoba@gmail.com>
2024-02-22 19:24:09 -0600

    Array Cyclic Rotation

---
+++ cyclicRotation_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestCyclicRotation(t *testing.T) {
+	tests := []struct {
+		input    []int
+		rotation int
+		expected []int
+	}{
+		{[]int{}, 1, []int{}},
+		{[]int{3, 8, 9, 7, 6}, 3, []int{9, 7, 6, 3, 8}},
+	}
+
+	for _, test := range tests {
+		result := CyclicRotation(test.input, test.rotation)
+		if slices.Compare(result, test.expected) != 0 {
+			t.Errorf("CyclicRotation(%v, %v) = %v, expected %v", test.input, test.rotation, result, test.expected)
+		}
+	}
+}
