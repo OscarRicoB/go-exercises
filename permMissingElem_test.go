8514f41a5075e60be60835b542219405302d6aed
Oscar Rico Barajas <oscarricoba@gmail.com>
2024-02-22 21:20:15 -0600

    Find missing elem on list

---
+++ permMissingElem_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestPermMissingElem(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected int
+	}{
+		{[]int{1, 2, 3, 4, 6, 7, 8}, 5},
+		{[]int{2, 3, 1, 5}, 4},
+	}
+
+	for _, test := range tests {
+		result := PermMissingElem(test.input)
+		if result != test.expected {
+			t.Errorf("FrogJmp(%v) = %v, expected %v", test.input, result, test.expected)
+		}
+	}
+
+}
