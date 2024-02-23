c5c00261ca983f6ac2e141fbd2b7f17cfa67231e
Oscar Rico Barajas <oscarricoba@gmail.com>
2024-02-22 18:35:55 -0600

    Binary Gap

---
+++ binaryGap_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestBinaryGap(t *testing.T) {
+	tests := []struct {
+		input    int
+		expected int
+	}{
+		{1041, 5},
+		{15, 0},
+		{32, 0},
+		{6, 0},
+		{561892, 3},
+	}
+
+	for _, test := range tests {
+		result := BinaryGap(test.input)
+		if result != test.expected {
+			t.Errorf("BinaryGap(%v) = %v, expected %v", test.input, result, test.expected)
+		}
+	}
+}
