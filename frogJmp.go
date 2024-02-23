0712c6730eb698c0c22038477a93b619b01f9a74
Oscar Rico Barajas <oscarricoba@gmail.com>
2024-02-22 21:06:46 -0600

    Frog jump

---
+++ frogJmp.go
@@ -0,0 +1,35 @@
+package main
+
+// A small frog wants to get to the other side of the road. The frog is currently located
+// at position X and wants to get to a position greater than or equal to Y. The small frog
+// always jumps a fixed distance, D.
+
+// Count the minimal number of jumps that the small frog must perform to reach its target.
+
+// Write a function:
+
+// func Solution(X int, Y int, D int) int
+
+// that, given three integers X, Y and D, returns the minimal number of jumps from position X
+// to a position equal to or greater than Y.
+
+// For example, given:
+
+//   X = 10
+//   Y = 85
+//   D = 30
+// the function should return 3, because the frog will be positioned as follows:
+
+// after the first jump, at position 10 + 30 = 40
+// after the second jump, at position 10 + 30 + 30 = 70
+// after the third jump, at position 10 + 30 + 30 + 30 = 100
+
+func FrogJmp(X int, Y int, D int) int {
+	steps := 0
+	distance := Y - X
+	steps = distance / D
+	if distance%D != 0 && D*steps < Y {
+		steps++
+	}
+	return steps
+}
---
+++ frogJmp_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestFrogJmp(t *testing.T) {
+	tests := []struct {
+		input1   int
+		input2   int
+		input3   int
+		expected int
+	}{
+		{10, 85, 30, 3},
+		{0, 100, 2, 50},
+		{10, 15, 300, 1},
+	}
+	for _, test := range tests {
+		result := FrogJmp(test.input1, test.input2, test.input3)
+		if result != test.expected {
+			t.Errorf("FrogJmp(%v, %v, %v) = %v, expected %v", test.input1, test.input2, test.input3, result, test.expected)
+		}
+	}
+}
